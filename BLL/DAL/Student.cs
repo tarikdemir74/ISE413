a80759a2494f6832fafb650320d41e347770f1a6
tarikdemir74 <153716511+tarikdemir74@users.noreply.github.com>
2024-10-14 14:46:11 +0300

    Assignment 1
    Student Class

---
+++ BLL/DAL/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DAL
+{
+	public class Student
+	{
+		private int ID { get; set; }
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+		public DateTime BirthDate { get; set; }
+		public double GPA { get; set; }
+
+
+	}
+}
